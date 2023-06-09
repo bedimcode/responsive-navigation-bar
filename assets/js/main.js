1ef0c869b549a97a9f0299088a8b2671ea3ccaef
Marlon <61248995+bedimcode@users.noreply.github.com>
2023-06-09 10:05:10 -0500

    Update main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,9 @@ const showMenu = (toggleId, navId) =>{
          nav = document.getElementById(navId)
 
    toggle.addEventListener('click', () =>{
-       // Agregar clase show-menu a nav menu
+       // Add show-menu class to nav menu
        nav.classList.toggle('show-menu')
-       // Agregar show-icon para mostrar y ocultar el icono del menú
+       // Add show-icon to show and hide menu icon
        toggle.classList.toggle('show-icon')
    })
 }
@@ -16,36 +16,36 @@ showMenu('nav-toggle','nav-menu')
 /*=============== SHOW DROPDOWN MENU ===============*/
 const dropdownItems = document.querySelectorAll('.dropdown__item')
 
-// 1. Selecionar cada dropdown item
+// 1. Select each dropdown item
 dropdownItems.forEach((item) =>{
     const dropdownButton = item.querySelector('.dropdown__button') 
 
-    // 2. Selecionar cada click del botón
+    // 2. Select each button click
     dropdownButton.addEventListener('click', () =>{
-        // 7. Seleccionar la clase show-dropdown actual
+        // 7. Select the current show-dropdown class
         const showDropdown = document.querySelector('.show-dropdown')
         
-        // 5. Llamar a la funcion toggleItem
+        // 5. Call the toggleItem function
         toggleItem(item)
 
-        // 8. Remover la clase show-dropdown de otros items
+        // 8. Remove the show-dropdown class from other items
         if(showDropdown && showDropdown!== item){
             toggleItem(showDropdown)
         }
     })
 })
 
-// 3. Crear una función para mostrar el dropdown
+// 3. Create a function to display the dropdown
 const toggleItem = (item) =>{
-    // 3.1. Selecionar cada dropdown content
+    // 3.1. Select each dropdown content
     const dropdownContainer = item.querySelector('.dropdown__container')
 
-    // 6. Si el mismo item contiene la clase show-dropdown, remover
+    // 6. If the same item contains the show-dropdown class, remove
     if(item.classList.contains('show-dropdown')){
         dropdownContainer.removeAttribute('style')
         item.classList.remove('show-dropdown')
     } else{
-        // 4. Agregar el height maximo al dropdown content y agregar la clase show-dropdown
+        // 4. Add the maximum height to the dropdown content and add the show-dropdown class
         dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
         item.classList.add('show-dropdown')
     }
@@ -55,16 +55,16 @@ const toggleItem = (item) =>{
 const mediaQuery = matchMedia('(min-width: 1118px)'),
       dropdownContainer = document.querySelectorAll('.dropdown__container')
 
-// Función para eliminar estilos desplegables en modo móvil cuando el navegador cambia de tamaño
+// Function to remove dropdown styles in mobile mode when browser resizes
 const removeStyle = () =>{
-    // Validar si la media query llega a 1118px
+    // Validate if the media query reaches 1118px
     if(mediaQuery.matches){
-        // Removemos el estilo de height de dropdown container
+        // Remove the dropdown container height style
         dropdownContainer.forEach((e) =>{
             e.removeAttribute('style')
         })
 
-        // Removemos la clase show-dropdown de dropdown item
+        // Remove the show-dropdown class from dropdown item
         dropdownItems.forEach((e) =>{
             e.classList.remove('show-dropdown')
         })
